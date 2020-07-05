4d2b810ab513ad1750afc2c42bd1301faf6f74bc
luccacarlini123 <lucca.carlini18@gmail.com>
2020-07-05 03:40:33 -0300

    Implementado interceptor para incluir token nas requisições

--- src/services/dto/cliente.service.ts
+++ src/services/dto/cliente.service.ts
@@ -1,28 +1,23 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { StorageService } from "../storage.service";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
-import { Observable } from "rxjs";
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
-export class ClienteService{
+export class ClienteService {
 
-    constructor(public http: HttpClient, public storage: StorageService){
+    constructor(public http: HttpClient, public storage: StorageService) {
     }
 
-    findByEmail(email: string){
-        
-        let token = this.storage.getLocalUser().token;
-        let authHeader = new HttpHeaders({'Authorization': 'Bearer '+token});
-
+    findByEmail(email: string) {
         return this.http.get<ClienteDTO>(
-            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-            {'headers': authHeader});
+            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
-    getImageFromBucket(id: string) : Observable<any>{
+    getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`;
-        return this.http.get(url, {responseType: 'blob'});
+        return this.http.get(url, { responseType: 'blob' });
     }
 }
